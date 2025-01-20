b5143eb8650281b5f9322c1a1cfcaf43d15a614f
gabmoleiro <gabriellabmoleiro@gmail.com>
2025-01-19 23:29:59 -0300

    feat: adiciona função para ligar todos os LEDs e atualiza o processamento de comandos

--- U4-T2-gpio-uart-control.c
+++ U4-T2-gpio-uart-control.c
@@ -11,11 +11,12 @@
 
 // Declaração de funções
 void configurar_leds(); //Configuracao inicial dos GPIOS - TODO - JULIERME E MARIO
+void ligar_todos_leds(); // GABRIELLA
 void desligar_todos_leds(); // TODO - JULIERME E MARIO
 void ligar_led(uint led_pin); //OK - GEISON
 void ligar_branco(); //TODO - JULIERME E MARIO
-void aciona_buzzer(); //OK! - Emyle
-void activate_buzzer_com_frequencia(int frequencia, int duracao_ms); //TODO - ALEXANDRE
+void acionar_buzzer(); //OK! - Emyle
+void acionar_buzzer_com_frequencia(int frequencia, int duracao_ms); //TODO - ALEXANDRE
 void processar_comandos(char comando); //OK - GEISON - TODO - GABRIELLA
 
 // Desenvolvido por Geison
@@ -27,11 +28,20 @@ void ligar_led(uint led_pin) {
 
 // Desenvolvido por Emyle
 // Função para acionar o buzzer por 3 segundos
-void aciona_buzzer(){
+void acionar_buzzer(){
     gpio_put(BUZZER, true);
     sleep_ms(3000);
     gpio_put(BUZZER, false);
     sleep_ms(3000);
+}
+
+// Desenvolvido por Gabriella
+// Função para ligar todos os LEDs
+void ligar_todos_leds() {
+    gpio_put(LED_VERDE, true);
+    gpio_put(LED_AZUL, true);
+    gpio_put(LED_VERMELHO, true);
+    printf("Todos os LEDs ligados.\n");
 }
 
 // Função para processar comandos UART
@@ -50,6 +60,23 @@ void processar_comandos(char comando) {
             ligar_led(LED_VERMELHO);
             printf("LED vermelho ligado.\n");
             break;
+        case '4': // Ligar todos os LEDs
+            ligar_todos_leds();
+            break;
+        case '5': // Desligar todos os LEDs
+            desligar_todos_leds();
+            break;
+        case '6': // Acionar buzzer por 3 segundos
+            acionar_buzzer();
+        case '7': // Acionar buzzer com frequência
+            acionar_buzzer_com_frequencia(2, 3000); // Frequência de 2 Hz por 3 segundos
+            break;
+        case '8': // Habilitar modo de gravação
+            reset_usb_boot(0, 0);
+            break;
+        default:
+            printf("Comando desconhecido.\n");
+            break;
     }
 }
 
@@ -60,6 +87,6 @@ int main()
     gpio_set_dir(BUZZER, GPIO_OUT);
 
     while (true) {
-        aciona_buzzer();
+        acionar_buzzer();
     }
 }
